f38c193e8fea6afa1761420835264f778dd7c2e0
Jarrod Bogard <jarrodbogard@gmail.com>
2024-04-17 16:16:56 -0400

    add weapon

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+// using System.Numerics;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRadius = 5f;
+    bool isProvoked = false;
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
 
     void Start()
     {
@@ -15,6 +21,45 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRadius)
+        {
+            isProvoked = true;
+            // navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+
+    }
+
+    void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log(this.name + " has caught and is attacking " + target.name);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
     }
 }
